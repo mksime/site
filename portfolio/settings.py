ff59ccc6f39d73f66b8430b566268b05a9276661
Kenji <ken@pop-os.localdomain>
2021-08-23 10:20:59 -0300

    Deploy API pronto 2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -65,6 +65,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
