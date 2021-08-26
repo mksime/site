22ebeaf27c0688345b664c271aa015973c60a220
Kenji <ken@pop-os.localdomain>
2021-08-25 23:30:32 -0300

    orreçoes relacionadas a arquivos estaticos

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
 from django.urls import path, include
+
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
