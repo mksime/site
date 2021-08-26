1c9518c954972796acb07b2d5f7fd4c1ff3182c1
Kenji <ken@pop-os.localdomain>
2021-08-25 23:20:50 -0300

    Alteraçoes de segurança

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -102,17 +102,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'apidb',
-#         'USER': 'ken',
-#         'PASSWORD': 'admin',
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
